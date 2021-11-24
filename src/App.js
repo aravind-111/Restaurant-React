663b63236b3e7a24f004e1624dfa3c4e9592db88
Aravindan-111 <aravindhan11195@gmail.com>
2021-11-24 20:17:03 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { css } from "@emotion/react";
+import Navbar from "./Components/Navbar";
+import PropagateLoader from "react-spinners/PropagateLoader";
+import Home from "./Components/Home";
+import Product from "./Components/Product";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
 
 function App() {
+  const [loading, setloading] = useState(false);
+
+  const override = css`
+    display: block;
+    border-color: #ff0505;
+    margin-top: 20%;
+  `;
+
+  useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 2000);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <PropagateLoader
+          color={"#3d2514 "}
+          Loading={loading}
+          css={override}
+          size={40}
+        />
+      ) : (
+        <>
+          <Navbar />
+          <Home />
+          <Product />
+          <About />
+          <Contact />
+        </>
+      )}
     </div>
   );
 }
